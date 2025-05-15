6cd8f352c15f6db07de188dc0be84c15fdae6662
Piyush Sawhney <piyushsawhney@outlook.com>
2025-05-15 12:48:51 +0530

    MOdifying models to have amfi scheme code as primary key for getting schemes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from data_retrieval.amfi_historical import process_five_year_nav
-from data_retrieval.amfi_schemes import update_amfi_schemes_to_db
+from data_retrieval.amfi_schemes import update_amfi_schemes_to_db, download_amfi_schemes
 from data_retrieval.nse_benchmarks import get_nse_benchmarks
 from data_retrieval.nse_benchmarks_tri import download_historical_tri
 from db import init_db
@@ -29,7 +29,8 @@ from db.engine import SessionLocal
 if __name__ == '__main__':
     init_db()
     session = SessionLocal()
+    # download_amfi_schemes("resources/nav.txt")
     update_amfi_schemes_to_db("resources/nav.txt")
-    process_five_year_nav("INF209K01YY7", "3", "119528", date(2025, 5, 2), date(2025, 5, 14))
-    get_nse_benchmarks()
-    download_historical_tri("NIFTY 50", date(2000, 5, 1), date(2025, 5, 14))
+    # process_five_year_nav("INF209K01YY7", "3", "119528", date(2025, 5, 2), date(2025, 5, 14))
+    # get_nse_benchmarks()
+    # download_historical_tri("NIFTY 50", date(2000, 5, 1), date(2025, 5, 14))
