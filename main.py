039162ff3f1f3490f696b0d934f2067483057d27
Piyush Sawhney <piyushsawhney@outlook.com>
2025-05-24 14:21:23 +0530

    Removing unnecessary files

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from data_retrieval.amfi.ir_performance.ir_performance_flow import retrieve_ir_performance_data
 from data_retrieval.amfi.mstar_url_update import update_scheme_urls
 from data_retrieval.mstar.metrics_flow import retrieve_scheme_metrics
 from db import init_db
@@ -28,8 +29,9 @@ if __name__ == '__main__':
     # update_amfi_schemes_to_db("resources/nav.txt")
     # update_launch_dates()
     # update_scheme_urls("resources/scheme_url.csv")
-    schemes = filer_by_urls_and_launch_dates()
-    for scheme in schemes:
-        print(f"AMFI Code: {scheme.amfi_code}, URL: {scheme.mstar_url}, Launch Date: {scheme.launch_date}")
-
-        retrieve_scheme_metrics(scheme.mstar_url, scheme.amfi_code, scheme.launch_date)
+    # schemes = filer_by_urls_and_launch_dates()
+    # for scheme in schemes:
+    #     print(f"AMFI Code: {scheme.amfi_code}, URL: {scheme.mstar_url}, Launch Date: {scheme.launch_date}")
+    #
+    #     retrieve_scheme_metrics(scheme.mstar_url, scheme.amfi_code, scheme.launch_date)
+    retrieve_ir_performance_data()
